6921068f945f200a2542d56fa6bfb965ec4f80ef
Carlosanamurov <carlosanamuroventura@upeu.edu.pe>
2021-07-12 10:54:56 -0500

    appcat

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth.routes'
 import userRoutes from './routes/user.routes'
 import cu2 from './routes/cu2.router'
 import cu3 from './routes/cu3.router'
+import aporteLogro from './routes/aportelogro.routes'
 const app = express();
 var cors = require('cors');
 
@@ -20,4 +21,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api/auth/users', userRoutes);
 app.use('/iteracion1/gest_usu_part', cu2);
 app.use('/iteracion1/gest_usu_eval', cu3);
+app.use('/api/auth/aportelogro', aporteLogro);
 export default app;
